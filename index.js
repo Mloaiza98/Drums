0dbf16a2cc8f00a6e448ce3148c5a525b9124674
Martin Loaiza W <107591792+Mloaiza98@users.noreply.github.com>
2022-12-25 15:11:09 -0500

    Adding files

---
+++ index.js
@@ -0,0 +1,76 @@
+var numBotones = document.querySelectorAll(".drum").length;
+console.log(numBotones);
+
+for (let index = 0; index < numBotones; index++) {document.querySelectorAll(".drum")[index].addEventListener("click" , function (){
+    var keyButton= this.innerHTML;
+    sounds(keyButton);
+    changeForm(keyButton);
+    
+    });
+
+    
+}
+
+
+document.addEventListener("keydown", function(event){
+    sounds(event.key);
+    changeForm(event.key);
+})
+// var audio = new Audio ("sounds/tom-1.mp3");
+// audio.play();
+
+function sounds(letter) {
+
+    switch (letter) {
+        case "w":
+            var tom1 = new Audio ("sounds/tom-1.mp3");
+            tom1.play();
+            
+            break;
+        case "a":
+            var tom2 = new Audio ("sounds/tom-2.mp3");
+            tom2.play();
+            
+        break;
+        case "s":
+            var tom3 = new Audio ("sounds/tom-3.mp3");
+            tom3.play();
+            
+        break;
+        case "d":
+            var tom4 = new Audio ("sounds/tom-4.mp3");
+            tom4.play();
+            
+        break;
+        case "j":
+            var snare = new Audio ("sounds/snare.mp3");
+            snare.play();
+            
+        break;
+
+        case "k":
+            var kick = new Audio ("sounds/kick-bass.mp3");
+            kick.play();
+            
+        break;
+
+        case "l":
+            var crash = new Audio ("sounds/crash.mp3");
+            crash.play();
+            
+        break;
+
+    
+        default:
+            break;
+    }
+    
+}
+
+function changeForm(botton) {
+    var activePart= document.querySelector("." + botton);
+    console.log(activePart)
+    activePart.classList.add("pressed");
+    setTimeout(function(){activePart.classList.remove("pressed")}, 100)
+    
+}
